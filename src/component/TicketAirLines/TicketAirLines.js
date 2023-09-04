d2a4b87fe013bd8c3ce80430003c15d3a78468b1
PODGORNY <oneminutesplease@gmail.com>
2023-09-04 20:21:59 +0300

    fix style

--- src/component/TicketAirLines/TicketAirLines.js
+++ src/component/TicketAirLines/TicketAirLines.js
@@ -8,8 +8,8 @@ import logoAir from '../image/Shuttle-logo.jpg';
 export const TicketAirLines = () => {
   return (
     <div className="ticketAirLines">
-      <div className="titleCards">
-        <div className="priceCards">13 400Р</div>
+      <div className="titleTicket">
+        <div className="priceTicket">13 400Р</div>
         <img className="logoAir" src={logoAir} alt="logo"></img>
       </div>
       <TicketBack />
