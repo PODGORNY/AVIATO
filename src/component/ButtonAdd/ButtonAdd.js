639588d418af3a748037d4397bcacbd4887498f1
PODGORNY <oneminutesplease@gmail.com>
2023-09-05 23:14:45 +0300

    fix animation

--- src/component/ButtonAdd/ButtonAdd.js
+++ src/component/ButtonAdd/ButtonAdd.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import './ButtonAdd.css';
+import classes from './ButtonAdd.module.css';
 
 export const Button = ({ onClick }) => {
+  const { add5Tickets } = classes;
   return (
-    <button className="add5Tickets" onClick={() => onClick()}>
+    <button className={add5Tickets} onClick={() => onClick()}>
       Еще 5 билетов!
     </button>
   );
